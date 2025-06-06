880005f53faaf5e45b6202a75a94991779159c11
jakmauu <dzakmau@gmail.com>
2025-06-06 20:18:27 +0700

    new

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
+    minify: 'esbuild'
   },
-  base: './',
-  publicDir: 'public'
-});
+  server: {
+    port: 3000
+  }
+})
